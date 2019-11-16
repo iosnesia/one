6e761e22c2a60ad370ef294d513af2de6862a076
Firda Sahidi <firdasahidi@hotmail.com>
2019-11-16 14:31:43 +0700

    Initial Commit

---
+++ LatihaniOSPertama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LatihaniOSPertama
+//
+//  Created by Firda Sahidi on 16/11/19.
+//  Copyright © 2019 Firda Sahidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LatihaniOSPertamaTests/LatihaniOSPertamaTests.swift
@@ -0,0 +1,34 @@
+//
+//  LatihaniOSPertamaTests.swift
+//  LatihaniOSPertamaTests
+//
+//  Created by Firda Sahidi on 16/11/19.
+//  Copyright © 2019 Firda Sahidi. All rights reserved.
+//
+
+import XCTest
+@testable import LatihaniOSPertama
+
+class LatihaniOSPertamaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
